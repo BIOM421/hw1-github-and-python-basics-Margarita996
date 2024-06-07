cc1733c5d286fe99242471a4a3c217980cb6737c
Margarita996 <2201111925@stu.pku.edu.cn>
2024-06-08 00:08:26 +0800

    hw1

--- ht.py
+++ ht.py
@@ -2,6 +2,3 @@ import hello
 
 def test_hello():
     assert hello.hello_world() == "Hello World!"
-    
-def test_Nhello():
-    assert hello.hello_world(3) == "Hello World!Hello World!Hello World!"
